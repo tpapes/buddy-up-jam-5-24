04e5fa5ee922363deea59bb668010f8973fa564e
tpapes <thenewdiamond209@gmail.com>
2024-05-04 14:44:40 -0700

    Inventory.gd ready for testing

--- dungeon crawler testenv/Inventory.gd
+++ dungeon crawler testenv/Inventory.gd
@@ -5,15 +5,18 @@ class_name Inventory
 @export var inventory_size:int = 100:
 	set(value):
 		inventory_size = value
-@export var item_list:Array[Item]
 
 var last_selected:int = 0
 
 func _ready():
 	connect("item_selected", select_item)
+	connect("item_activated", activate_item)
 
 func toggle():
 	visible = !visible
+	if visible and item_count > 0:
+		select(last_selected)
+		grab_focus()
 
 func _unhandled_input(event):
 	if event.is_action_pressed("ui_cancel"):
@@ -22,5 +25,9 @@ func _unhandled_input(event):
 		var item = load("res://Test_Item.tres")
 		add_item(item.get("item_name"), item.get("texture"))
 
-func select_item(item_ind):
+func select_item(item_ind:int):
+	last_selected = item_ind
+
+func activate_item(item_ind:int):
+	toggle()
 	print(item_ind)
