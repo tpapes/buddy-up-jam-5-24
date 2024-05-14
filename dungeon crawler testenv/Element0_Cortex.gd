83f239307ee0aa47bf59ed377ee676453bf88b33
QCortex07 <prcook2004@gmail.com>
2024-05-14 15:21:53 -0400

    more experimentation

---
+++ dungeon crawler testenv/Element0_Cortex.gd
@@ -0,0 +1,65 @@
+extends Node
+
+@onready var player = Node
+@onready var move_ready : bool
+@onready var targetPos : Vector2
+@onready var movePattern = [Vector2.RIGHT]
+@onready var moveStep : int
+@onready var groundCheck = $GroundCheck
+@onready var wallCheck = $WallCheck
+@onready var foot = $Foot
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	targetPos = self.position
+	moveStep = 0
+	move_ready = true
+	player = get_node("/root/TileStuff/Player_CTX")
+	if (player != null):
+		player.move.connect(_on_player_move)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	
+	# When the player is ready to move, determine the move direction for this step
+	if (player != null and move_ready and movePattern.size() > 0):
+		move_ready = false
+		self.position = targetPos
+		var direction = Vector2.ZERO
+		
+		for i in movePattern.size():
+			if (check_direction(targetPos, movePattern[moveStep] * 32)):
+				direction = movePattern[moveStep] * 32
+				break;
+			else:
+				movePattern[moveStep] *= -1
+				if (check_direction(targetPos, movePattern[moveStep] * 32)):
+					direction = movePattern[moveStep] * 32
+					break;
+				else:
+					moveStep = (moveStep + 1) % movePattern.size()
+		targetPos += direction
+		moveStep = (moveStep + 1) % movePattern.size()
+	
+	# Slide player into target position
+	self.position = lerp(self.position, targetPos, 16 * delta)
+	
+	# Move foot onto target position
+	foot.global_position = targetPos
+	foot.force_update_transform()
+	
+
+func check_direction(from, direction):
+	# Check if there is ground
+	groundCheck.global_position = from + direction
+	groundCheck.target_position = -direction
+	groundCheck.force_raycast_update()
+	# Check if the spot is taken
+	wallCheck.global_position = from + direction
+	wallCheck.target_position = -direction
+	wallCheck.force_raycast_update()
+	# Return true if there is ground and spot is not taken
+	return groundCheck.is_colliding() && !wallCheck.is_colliding()
+
+func _on_player_move():
+	move_ready = true
--- dungeon crawler testenv/GDScrAttempt_Cortex.gd
+++ dungeon crawler testenv/GDScrAttempt_Cortex.gd
@@ -1,16 +1,22 @@
 extends Node
 
-@onready var targetPos = $Vector2
-@onready var inputLog = $Vector2
-@onready var move_ready = $bool
-@onready var ray = $RayCast2D
-@onready var foot = $Area2D
+@onready var targetPos : Vector2
+@onready var inputLog : Vector2
+@onready var move_ready : bool
+@onready var move_timer : float
+@onready var move_fire : bool
+@onready var groundCheck = $GroundCheck
+@onready var wallCheck = $WallCheck
+@onready var foot = $Foot
+signal move
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	targetPos = self.position
 	inputLog = Vector2.ZERO
 	move_ready = true
+	move_timer = 0.0
+	move_fire = true
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -27,6 +33,7 @@ func _process(delta):
 	
 	# When the player is ready to move, determine the move direction for this step
 	if (move_ready):
+		
 		var direction = Vector2.ZERO
 		# The point of this breakdown is so that inputs aren't dropped due to my arbitrary choice in
 		# which direction is checked first
@@ -36,12 +43,16 @@ func _process(delta):
 			direction.x += sign(inputLog.x) * 32
 			inputLog.x -= sign(inputLog.x)
 			move_ready = false
+			move_timer = 0.0
+			move_fire = false
 		# Then check Y direction
 		elif (inputLog.y != 0 and check_direction(targetPos, Vector2.DOWN * sign(inputLog.y) * 32)):
 			self.position = targetPos
 			direction.y += sign(inputLog.y) * 32
 			inputLog.y -= sign(inputLog.y)
 			move_ready = false
+			move_timer = 0.0
+			move_fire = false
 		# Only forget inputs if neither direction passes
 		else:
 			inputLog = Vector2.ZERO
@@ -51,13 +62,28 @@ func _process(delta):
 	self.position = lerp(self.position, targetPos, 16 * delta)
 	
 	# Determine if the player is ready to move on next update
-	move_ready = self.position.distance_to(targetPos) < 4
+	move_ready = move_timer <= 0.0
 	
 	# Move foot onto target position
 	foot.global_position = targetPos
+	foot.force_update_transform()
+	
+	# Signal Elements to move
+	if (move_timer <= 0.0 / 2.0 and !move_fire):
+		move.emit()
+		move_fire = true
+	
+	# move_fire is basically a timer
+	if (move_timer > 0.0): move_timer -= delta
 
 func check_direction(from, direction):
-	ray.global_position = from + direction
-	ray.target_position = -direction
-	ray.force_raycast_update()
-	return ray.is_colliding()
+	# Check if there is ground
+	groundCheck.global_position = from + direction
+	groundCheck.target_position = -direction
+	groundCheck.force_raycast_update()
+	# Check if the spot is taken
+	wallCheck.global_position = from + direction
+	wallCheck.target_position = -direction
+	wallCheck.force_raycast_update()
+	# Return true if there is ground and spot is not taken
+	return groundCheck.is_colliding() && !wallCheck.is_colliding()
