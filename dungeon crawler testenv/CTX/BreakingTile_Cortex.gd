f017c441538f468daed0370365dd8f4d6b01a369
QCortex07 <prcook2004@gmail.com>
2024-05-17 21:46:01 -0400

    added sprites to falling platforms

--- dungeon crawler testenv/CTX/BreakingTile_Cortex.gd
+++ dungeon crawler testenv/CTX/BreakingTile_Cortex.gd
@@ -1,15 +1,22 @@
 extends Node
 
-@onready var coll = $Area2D
-@onready var stepped = $bool
+@onready var coll : Area2D
+@onready var stepped : bool
 @onready var area = $Area2D
+@onready var shape
+@onready var collShape = $Area2D/CollisionShape2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	coll = null
 	stepped = false
-	area = self
-	pass # Replace with function body.
+	shape = self
+	
+	var s = RectangleShape2D.new()
+	s.set_size(shape.size - Vector2(0,32))
+
+	collShape.shape = s
+	collShape.get_node(".").position = s.size / 2
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -24,5 +31,3 @@ func _process(delta):
 			
 	if (stepped and !found):
 		queue_free()
-		
-	pass
