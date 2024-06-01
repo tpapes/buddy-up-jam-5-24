d08222d219085859c1ff72c968eaf957744df46e
Dustin Garner <144393035+dustingarner@users.noreply.github.com>
2024-06-01 08:49:07 -0500

    I tried refactoring but something isn't working
    I wanted to make the big fracture script inherit from the other but I messed up something with the shaking in the process so for now ignore this branch.

--- dungeon crawler testenv/CTX/FracPoint_Cortex.gd
+++ dungeon crawler testenv/CTX/FracPoint_Cortex.gd
@@ -1,6 +1,5 @@
 extends Sprite2D
-
-enum ShakeStates {IDLE = 0, FIRST, SECOND, THIRD}
+class_name FracturePoint
 
 @export var health:= 1
 
@@ -11,13 +10,7 @@ enum ShakeStates {IDLE = 0, FIRST, SECOND, THIRD}
 
 signal just_broke
 
-const SHAKE_LENGTH:= 2
-const SHAKE_SPEED:= 40
-const SCALE_DIMINISH_FACTOR:= 0.96
-
 var is_broken:= false
-var shaking:= 0
-var original_scale: Vector2
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -27,24 +20,17 @@ func _ready():
 	for particle in particles:
 		particle.one_shot = true
 		particle.emitting = false
-	original_scale = scale
 
 func attempt_undo(prev_health: int):
 	if prev_health == 0 and health > 0:
 		break_frac()
 	elif prev_health > 0 and health == 0:
 		unbreak_frac()
-	if prev_health == health + 1:
-		scale /= SCALE_DIMINISH_FACTOR
-		collision.scale *= SCALE_DIMINISH_FACTOR
 	health = prev_health
 
 func break_frac():
 	health -= 1
 	if health > 0:
-		shaking = ShakeStates.FIRST
-		scale *= SCALE_DIMINISH_FACTOR
-		collision.scale /= SCALE_DIMINISH_FACTOR
 		return
 	var spr : Sprite2D = self.get_node(".")
 	var p = spr.region_rect.position + Vector2.RIGHT * 32
@@ -53,10 +39,6 @@ func break_frac():
 	self.z_index = 0
 	self.name = "Broken_FracPoint_CTX"
 	is_broken = true
-	offset.x = 0
-	shaking = 0
-	scale = original_scale
-	collision.scale = Vector2.ONE
 	for particle in particles:
 		particle.emitting = true
 	just_broke.emit()
@@ -70,21 +52,3 @@ func _on_drill_break(frac_area):
 		break_frac()
 		#self.reparent(get_parent().get_parent())
 		#print("borken")
-
-func shake_process(delta):
-	if shaking == ShakeStates.IDLE:
-		return
-	var direction = -1
-	if shaking == ShakeStates.SECOND:
-		direction = 1
-	offset.x += delta * direction * SHAKE_SPEED
-	if offset.length() > SHAKE_LENGTH and (shaking == ShakeStates.FIRST or \
-			shaking == ShakeStates.SECOND):
-		shaking += 1
-		return
-	if offset.length() < 0.5 and shaking == ShakeStates.THIRD:
-		shaking = ShakeStates.IDLE
-
-func _process(delta):
-	shake_process(delta)
-
